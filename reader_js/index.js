4475a605aba0c8ec6fc127d1b174ebb031849f21
Simone Cattani <simone3.cattani@gmail.com>
2019-06-14 10:12:18 +0200

    readers

--- reader_js/index.js
+++ reader_js/index.js
@@ -1,7 +1,5 @@
 var avro = require('avro-js');
 
-var type = avro.parse('data/schema.avsc');
-
 avro.createFileDecoder('data/sample-avro/part-0001.avro')
   .on('metadata', function (type) { /* `type` is the writer's type. */ })
   .on('data', function (record) { console.log(record) });
--- reader_python/main.py
+++ reader_python/main.py
@@ -2,8 +2,6 @@ import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
 
-schema = avro.schema.Parse(open('data/schema.avsc', "r").read())
-
 reader = DataFileReader(open("data/sample-avro/part-0001.avro", "rb"), DatumReader())
 for row in reader:
     print(row)
